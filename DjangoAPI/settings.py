05a00290c8b910f65c5ef4be2df331f18ba20d18
Freddy Rios <freddy_stumpfs@hotmail.com>
2023-04-13 23:02:49 -0400

    deploy2_13/04/23

--- DjangoAPI/settings.py
+++ DjangoAPI/settings.py
@@ -160,7 +160,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# desactivamos este campo como testing
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
